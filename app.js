3620277b2d8029f925369cc1e4a6f0ddc8a0f232
BADIDDO <aymenmatrix311@gmail.com>
2022-01-06 22:18:57 +0100

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener("click" , () => {
+    document.body.style.background = "#000000cc";
+    document.body.style.color = "white";
+})
